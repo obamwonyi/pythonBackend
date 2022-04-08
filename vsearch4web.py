90de6a849c44acf451e8cbc560a6351e56fcc280
obamwonyi <developer.oduwa@gmail.com>
2022-04-07 21:07:25 -0400

    The log data are now in a table form

--- vsearch4web.py
+++ vsearch4web.py
@@ -1,35 +1,54 @@
-#importing the flask method
-from flask import Flask,render_template,request
-#importing the search4letters method
+# importing the flask method
+from flask import Flask, render_template, request, escape
+# importing the search4letters method
 from vsearch import search4letters
 
-#instantiating the Flask class  
+# instantiating the Flask class
 app = Flask(__name__)
 
 
-# the hello function will be returning a string 
-@app.route('/search4',methods = ['POST'])
+def log_request(req: 'flask_request', res: str) -> None:
+    with open('vsearch.log', 'a') as log:
+        print(req.form, req.remote_addr, req.user_agent, res, file=log, sep="|")
 
-#function with the type hint of string 
-#to return the value of a searched string 
-def do_search() -> str : 
+
+@app.route('/search4', methods=['POST'])
+# function with the type hint of string
+# to return the value of a searched string
+def do_search() -> str:
     phrase = request.form['phrase']
     letters = request.form['letters']
     title = "Here are your results:"
-    results = str(search4letters(phrase,letters))
+    results = str(search4letters(phrase, letters))
+    log_request(request, results)
+    return render_template('results.html', the_phrase=phrase,
+                           the_letters=letters,
+                           the_title=title,
+                           the_results=results)
 
-    return render_template('results.html',the_phrase = phrase,
-            the_letters = letters,
-            the_title = title,
-            the_results =results)
+# making the root directory / kind of full through to entry
 
-#making the root directory / kind of full through to entry
+
 @app.route('/')
 @app.route('/entry')
-def entry_page() :
-    return render_template('entry.html',the_title ="Welcome to seach4letters on the web!")
+def entry_page():
+    return render_template('entry.html', the_title="Welcome to seach4letters on the web!")
+
+
+@app.route('/viewlog')
+def view_the_log() -> 'html':
+    contents = []
+    with open('vsearch.log') as log:
+        for line in log:
+            contents.append([])
+            for item in line.split('|'):
+                contents[-1].append(escape(item))
+    titles = ('Form Data', 'Remote_addr', 'User_agent', 'Results')
+    return render_template('viewlog.html',
+                           the_title='View Log',
+                           the_row_titles=titles,
+                           the_data=contents,)
 
 
 if __name__ == '__main__':
     app.run(debug=True)
-    
